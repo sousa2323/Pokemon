b440c0aba3c959dc4ab79bd2d63accf043efbbde
sousa2323 <sousanot.23@gmail.com>
2023-07-10 13:07:38 -0300

    adicionada argumentos não obrigatorios

--- pokemon.py
+++ pokemon.py
@@ -1,11 +1,20 @@
 class Pokemon:
-    def __init__(self, tipo, especie):
+    def __init__(self, tipo, especie, level=1, nome=None):
         self.tipo = tipo
         self.especie = especie
+        self.level = level
+
+        if nome:
+            self.nome = nome
+        else:
+            self.nome = especie    
 
     def __str__(self):
-        return self.especie   
+        return "{}({})".format(self.especie, self.level)
 
-meu_pokemon = Pokemon('fogo', 'charmander')
+    def atacar(self, pokemon):
+        print("{} atacou {}".format(self, pokemon))
 
-print(meu_pokemon)        
+meu_pokemon = Pokemon('fogo', 'charmander', nome="gui", level=50)
+
+print(meu_pokemon)
