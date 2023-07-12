b08e0e0f6fb4a5ec4be0bb8f8bc464d33cd3b20a
sousa2323 <sousanot.23@gmail.com>
2023-07-12 12:51:06 -0300

    APLICANDO RNG

--- pokemon.py
+++ pokemon.py
@@ -21,8 +21,10 @@ class Pokemon:
         return "{}({})".format(self.especie, self.level)
 
     def atacar(self, pokemon):
-        pokemon.vida = pokemon.vida - self.ataque
-        print("{} perdeu {} pontos de vida".format(pokemon, self.ataque))
+        ataque_efetivo = int((self.ataque * random.random() * 1.3))
+        pokemon.vida -= pokemon.vida - ataque_efetivo
+     
+        print("{} perdeu {} pontos de vida".format(pokemon, ataque_efetivo))
 
         if pokemon.vida <= 0:
             print("{} foi derrotado".format(pokemon))
